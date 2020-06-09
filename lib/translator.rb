ba1230c2c02a285e6b9f8a73d91e7b0ef365d959
Karem Ceron <karemiceron@gmail.com>
2020-06-09 00:55:31 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,32 +1,44 @@
 # require modules here
+require 'pry' 
 require "yaml"
 
 def load_library(file_path)
-  emoticon_hash = YAML.load_file('./lib/emoticons.yml')
-  
-  emoticon_hash.each
-  
-  [:foo, :bar, :jazz].inject({}) do |hash, item|
-  hash[item] = item.to_s.upcase
-  hash
-end
- => {:foo=>"FOO", :bar=>"BAR", :jazz=>"JAZZ"}
-  
-end 
-=begin 
-  new_hash = { "get_language" => {}, "get_emoticon"  => {}}
-  
+  emoticon_hash = YAML.load_file(file_path)
+  new_hash = {}
   emoticon_hash.each do |key, value|
-    new_hash["get_language"][value[1]] = key 
-    new_hash ["get_emoticon"][value[0]] = value[1]
-  end 
+    new_hash[key] = {:english => value[0], :japanese => value[1]}
+  end
   new_hash
-=end 
+end 
 
-def get_japanese_emoticon
-  # code goes here
-end
+def get_english_meaning(file_path, emoticon)
+  emoticon_hash = load_library(file_path)
+  result = nil 
+  
+  emoticon_hash.map do |key, value|
+    if emoticon_hash.has_value?(emoticon)
+      result = emoticon_hash[value][0]
+      return result
+    elsif 
+      result == nil 
+      return "Sorry, that emoticon was not found"
+    end 
+  end 
+end 
 
-def get_english_meaning
-  # code goes here
+
+def get_japanese_emoticon(file_path, emoticon)
+  emoticon_hash = load_library(file_path)
+  message = "Sorry, that emoticon was not found"
+  result = nil 
+  
+  emoticon_hash.map do |key, value|
+    if emoticon_hash.value?(emoticon)
+      result << emoticon_hash[value][1]
+      return result
+    elsif 
+      result == nil 
+      return message 
+    end 
+  end 
 end
