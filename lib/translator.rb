4cae8c8da5bf65a619e843b7dc4c5e901bb99be9
Karem Ceron <karemiceron@gmail.com>
2020-06-09 15:02:57 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,32 +11,24 @@ def load_library(file_path)
   new_hash
 end 
 
-def get_english_meaning(file_path, emoticon)
+def get_english_meaning(file_path, japanese_emoticon)
   emoticon_hash = load_library(file_path)
-  result = nil 
-  
-  emoticon_hash.map do |key, value|
-    if emoticon_hash.has_value?(emoticon)
-      result = emoticon_hash[value][0]
-      return result
-    elsif 
-      result == nil 
-      return "Sorry, that emoticon was not found"
+ 
+  emoticon_hash.each do |english_meaning, inner_hash|
+      if inner_hash[:japanese] == japanese_emoticon
+        return english_meaning
+      end 
     end 
-  end 
+    "Sorry, that emoticon was not found"
 end 
 
-def get_japanese_emoticon(file_path, emoticon)
+def get_japanese_emoticon(file_path, english_emoticon)
   emoticon_hash = load_library(file_path)
-  result = nil 
   
-  emoticon_hash.map do |key, value|
-    if emoticon_hash.value?(emoticon)
-      result << emoticon_hash[value][1]
-      return result
-    elsif 
-      result == nil 
-      return "Sorry, that emoticon was not found"
+  emoticon_hash.each do |english_meaning, inner_hash|
+    if inner_hash[:english] == english_emoticon
+      return inner_hash[:japanese]
     end 
   end 
-end
+  "Sorry, that emoticon was not found"
+end 
